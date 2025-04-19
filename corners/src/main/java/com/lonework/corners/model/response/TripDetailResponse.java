71dfde8684b649a7a65e89aa9601329e6e0024a1
Vojtech FIser <fiser.vojta1@gmail.com>
2025-04-19 12:51:29 +0900

    updates

---
+++ corners/src/main/java/com/lonework/corners/model/response/TripDetailResponse.java
@@ -0,0 +1,33 @@
+package com.lonework.corners.model.response;
+
+import com.lonework.corners.model.Comment;
+import com.lonework.corners.model.Trip;
+import org.geotools.api.feature.simple.SimpleFeature;
+
+import java.util.List;
+
+
+public record TripDetailResponse(Long id,
+                                 String name,
+                                 String description,
+                                 String creator,
+                                 Integer duration,
+                                 Double rating,
+                                 SimpleFeature feature,
+                                 List<PlaceSimpleResponse> places,
+                                 List<Comment> comments) {
+    public TripDetailResponse(Trip trip, List<PlaceSimpleResponse> places, SimpleFeature feature) {
+        this(
+                trip.getId(),
+                trip.getName(),
+                trip.getDescription(),
+                trip.getCreator(),
+                trip.getDuration(),
+                trip.getRating(),
+                feature,
+                places,
+                null
+        );
+    }
+
+}
